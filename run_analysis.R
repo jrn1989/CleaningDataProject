935d1498a47c39322793a7f56e4659e455d9b7db
jrn <jose.robles.noriega@gmail.com>
2014-09-20 21:27:10 -0500

    Step 5 resdy

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 # Coded by Jose Robles
+
 library("plyr")
 dir = "~/Documents/DataScienceSpecialization/3-CleaningData/project/CleaningDataProject"
 setwd(dir)
@@ -18,9 +19,11 @@ names(subject_train) <- "subject"
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt", quote="\"")
 names(y_train) <- "activity"
 
-# Test set
+
 
 data_train <- cbind(subject_train, X_train, y_train)
+
+# Test set
 
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt", quote="\"")
 names(X_test) <- features$V2
@@ -54,3 +57,16 @@ currentNames <- gsub("Gyro", "Gyroscope", currentNames)
 currentNames <- gsub("Mag", "Magnitude", currentNames)
 
 names(data) <- currentNames
+
+data$subject <- as.factor(data$subject)
+
+data2<-aggregate(data[,2:(length(data)-2)], by=list(data$subject,data$activityLabel), FUN=mean)
+
+#data2<-aggregate(data[,2:3], by=list(data$subject,data$activityLabel), FUN=mean)
+currentNames <- names(data2)
+currentNames[1] <- "subject"
+currentNames[2] <- "activityLabel"
+names(data2) <- currentNames
+
+write.table(data,file="firstDataset.txt",row.names=FALSE)
+write.table(data2,file="secondDataset.txt",row.names=FALSE)
